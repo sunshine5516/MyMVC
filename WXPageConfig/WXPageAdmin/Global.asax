868ad18a348983ec41797e5eca3e33cd7c54a585
Jayson <961128502@qq.com>
2017-03-15 15:28:24 +0800

    test

--- WXPageConfig/WXPageAdmin/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WXPageAdmin.MvcApplication" Language="C#" %>
--- WXPageConfig/WXPageDomain/Concrete/EFDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using System.Data.Entity;
-namespace WXPageDomain.Concrete
-{
-    public class EFDbContext: DbContext
-    {
-    }
-}
